1bfb6802a9a6e871ca086465926a6b927343640b
GS-SoftCode <ggsotomayor@sudamericano.edu.ec>
2024-12-09 17:30:33 -0500

    Antes de crear rutas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoComponent } from './logo/logo.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogoComponent, CreateAccountComponent, LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MindSpace_App';
+  title = 'MindSpace-App';
 }
