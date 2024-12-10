f7ac32d164f76eb55a0d6376745a4d0e1152adc7
Daniel Campoverde <alx@sillybytes.net>
2024-12-09 20:35:10 -0500

    Agregar ruta page 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { Page1Component } from './pages/page1/page1.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'create-account', component: CreateAccountComponent}
+    {path: 'create-account', component: CreateAccountComponent},
+    {path: 'login', component: LogInComponent},
+    {path: 'page1', component: Page1Component}
 ];
